02403e8fad38f38135fd71d79b9396060a582d8c
dlmanrique <dl.manrique@uniandes.edu.co>
2025-07-29 21:10:11 +0000

    File for creating mini dataset version

--- CVS_Utils_adapters/create_mini_dataset_version.py
+++ CVS_Utils_adapters/create_mini_dataset_version.py
@@ -4,7 +4,11 @@
 import os
 import glob
 import json
+import pandas as pd
+import random
 
+
+random.seed(42)
 
 def subset_creation(path: str):
     """
@@ -14,7 +18,42 @@ def subset_creation(path: str):
 
     with open(path, 'r') as f:
         data = json.load(f)
-    breakpoint()
+
+    df_images = pd.DataFrame(data['images'])
+    df_annots = pd.DataFrame(data['annotations'])
+
+    selected_images = pd.DataFrame(columns=df_images.columns)
+    selected_annots = pd.DataFrame(columns=df_annots.columns)
+
+
+    for video_idx in df_images['video_name'].unique().tolist():
+
+        possible_frames = df_images[df_images['video_name'] == video_idx]['file_name'].tolist()
+        nums = random.sample(range(18), 2)
+        selected_frame_1 = possible_frames[nums[0]]
+        selected_frame_2 = possible_frames[nums[1]]
+
+        for image_name in [selected_frame_1, selected_frame_2]:
+            image_info = df_images[df_images['file_name'] == image_name]
+            selected_images = pd.concat([selected_images, image_info], ignore_index=True)
+
+            annot_info = df_annots[df_annots['image_name'] == image_name]
+            selected_annots = pd.concat([selected_annots, annot_info], ignore_index=True)
+
+    selected_images = selected_images.sort_values(by='file_name')
+    selected_annots = selected_annots.sort_values(by='image_name')
+    list_of_images = selected_images.to_dict(orient='records')
+    list_of_annots = selected_annots.to_dict(orient='records')
+
+    complete_data = {'images': list_of_images, 'annotations': list_of_annots}
+    file_name = os.path.basename(path)
+    dir_name = os.path.dirname(path)
+
+    with open(os.path.join(dir_name, f'mini_{file_name}'), 'w') as f:
+        json.dump(complete_data, f, indent=4)
+
+    print(f'Mini version of {file_name} created succesfully')
+
 
 
 if __name__ == '__main__':
